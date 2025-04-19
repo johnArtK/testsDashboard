5e2f8b0584e24a855d2eaddb596ce3e0cc38d2c8
john4rk <timoff50@gmail.com>
2025-04-19 16:58:09 +0300

    add db local(without getById)

--- src/entities/test/api/siteApi.ts
+++ src/entities/test/api/siteApi.ts
@@ -2,7 +2,7 @@ import { BASE_URL } from "../../../shared/configApi/baseUrl"
 import { Site } from "../model/types"
 
 export async function fetchSites(): Promise<Site[]> {
-  const response = await fetch(`${BASE_URL}/sites`)
+  const response = await fetch(`${BASE_URL}/sites.json`)
   if (!response.ok) {
     throw new Error(`Failed to fetch: ${response.status}`)
   }
--- src/entities/test/api/testApi.ts
+++ src/entities/test/api/testApi.ts
@@ -2,13 +2,14 @@ import { BASE_URL } from "../../../shared/configApi/baseUrl"
 import { Test } from "../model/types"
 
 export async function fetchTests(): Promise<Test[]> {
-  const response = await fetch(`${BASE_URL}/tests`)
+  const response = await fetch(`${BASE_URL}/tests.json`)
   if (!response.ok) {
     throw new Error(`Failed to fetch: ${response.status}`)
   }
   return response.json()
 }
 
+//API нет, поэтому по Id не работает(((
 export async function fetchTestById(id: number): Promise<Test> {
   const response = await fetch(`${BASE_URL}/tests/${id}`)
   return response.json()
